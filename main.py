d8ef9992d6c8c1f3a65a7ef043f511e94e85c7d8
rukies01 <132057787+rukies01@users.noreply.github.com>
2025-11-14 23:06:13 +0100

    Fixed selection function and improved menu

--- main.py
+++ main.py
@@ -16,23 +16,47 @@ Wardrope = {'Coporate': ['Navy blue trouser suit','Black skirt suit', 'Navyblue 
              'Native':['Ankara Jumpsuit', 'Ankara Baggy Pant and Top','Brocade Bubu Gown' ],
 
             }
+Laundry = []
+Favorites = []
 
-def selection():
-    print('Welcome to your Wardrope')
-    print('Available selections are :')
-    for key in Wardrope:
-     print (key)
-    select = input('What kind of outfit would you like today : ')
+def main():
+    while True:
+        print('Welcome to your Wardrop')
+        print('What will you like to do ?')
+        print('1. Select a Cloth')
+        print('2. Add cloth to favorites')
+        print('3. View Favorites') 
+        print('4. Empty my Laundry')
+        print('5. Add cloth to wardrope')
+        print('6.Exit')
+
+        user_choice = input()
+        if user_choice == '1':
+            item = selection()
+            if item:
+        
+def choice(answer):
+    my_choice = input('Would you like me to add this to your Favorites ? (yes/no): ')
+    if my_choice == 'yes':
+          Favorites.append(answer)  # add 'answer' to favorites list
+          print('Item added to your Favorites!')
+    else:
+          print('Item not added to Favorites!')  # print this if my_choice is no
+
 
 def selection2(select):
    if select == 'Coporate':
-        print(random.choice(Wardrope['Coporate']))
+        answer = random.choice(Wardrope['Coporate'])  #returns a random choice from coporate
+        print(answer)
    elif select == 'Casual':
-        print(random.choice(Wardrope['Casual']))
+        answer = random.choice(Wardrope['Casual']) # #returns a random choice from casual
+        print(answer)
    elif select == 'Sport':
-        print(random.choice(Wardrope['Sport']))
+        answer = random.choice(Wardrope['Sport']) # #returns a random choice from sport
+        print(answer)
    elif select == 'Native':
-        print(random.choice(Wardrope['Native']))
+        answer = random.choice(Wardrope['Native']) # #returns a random choice from native
+        print(answer)
    else:
        try:
             if select not in ['Coporate','Casual','Sport','Native']:
@@ -41,6 +65,35 @@ def selection2(select):
            print(error)
 
 
+def selection():
+    
+    for key in Wardrope:
+     print (key)
+    select = input('What kind of outfit would you like today : ')  # print available options
+    answer = selection2(select)
+    choice(answer)
+
+
+    
+def view_favorites():
+     print(f'Your Favorites : {Favorites}')  # this shows a list of clothes added to favorites
+
+
+def add_cloth_to_laundry(answer):
+    while True:
+        Laundry.append(answer)
+    else:
+        break
+        
+
+
+def empty_laundry():
+    Laundry.clear()  # this empty/clear the laundry list
+
+
+
+
+
 selection()
-selection2('Coporate')
+
                    
